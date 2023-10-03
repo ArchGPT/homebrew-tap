7008700664836eec749ad3ba2b24c162b19d0b73
archy <a@0a.io>
2023-10-04 02:39:21 +0800

    go

--- insomnium.rb
+++ insomnium.rb
@@ -1,14 +1,21 @@
 class Insomnium < Formula
   desc "a fast local API testing tool that is privacy-focus and 100% local. For testing GraphQL, REST, WebSockets and gRPC. This is a fork of insomnia"
   homepage "https://github.com/archGPT/insomnium"
-  url "https://github.com/ArchGPT/insomnium/releases/download/core%400.1.3/Insomnium.Core-0.1.3.tar.gz"
-  version "0.1.2"
-  sha256 "4f0d376934fe3a9e63a947b1ba8ad0aa7f1ae7145ec349a9ae814ac8807d0db5"
+  url "https://github.com/ArchGPT/insomnium/archive/refs/tags/core@0.1.3.tar.gz"
+  version "1.0.0"
+  sha256 "97de94ee823ecea9c8bbb483dcb4e67514161b3e968e7feaba91c94e6aebb69a"
 
   depends_on "node" => :build
 
   def install
+    system "npm", "run", "app-package"
     system "mv", "./dist/mac-universal/Insomnium.app", "/Applications"
+  end
+
+  def caveats
+    <<~EOS
+      Insomnium is installed in /Applications.
+    EOS
   end
 
 end
